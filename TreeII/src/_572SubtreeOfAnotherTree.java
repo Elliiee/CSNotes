b6b1c94b7e8a07afa1609583aa1814d69bf1cc51
Elliiee <danyiluozhang@gmail.com>
2020-04-01 16:36:44 -0500

    572

--- TreeII/src/_572SubtreeOfAnotherTree.java
+++ TreeII/src/_572SubtreeOfAnotherTree.java
@@ -1,2 +1,18 @@
 public class _572SubtreeOfAnotherTree {
+    public boolean isSubTree(TreeNode s, TreeNode t){
+        if (s == null) return false;
+
+        return isSubtreeWithRoot(s, t) || isSubTree(s.left, t) || isSubTree(s.right, t);
+    }
+
+    private boolean isSubtreeWithRoot(TreeNode s, TreeNode t){
+        if (t == null && s == null) return true;
+        if (t == null || s == null) return false;
+        if (t.val != s.val) return false;
+
+        return isSubTree(s.left, t.left) && isSubtreeWithRoot(s.right, t.right);
+    }
 }
+/*
+check if tree t is the subtree of tree s.
+ */
