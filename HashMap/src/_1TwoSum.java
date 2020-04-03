b27e855c4e5b4a1cec55d1c0d906c42c8b2b020a
Elliiee <danyiluozhang@gmail.com>
2020-04-03 15:54:06 -0500

    1

---
+++ HashMap/src/_1TwoSum.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+public class _1TwoSum {
+    public int[] twoSum(int[] nums, int target){
+        HashMap<Integer, Integer> indexForNum = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++){
+            if (indexForNum.containsKey(target - nums[i])){
+                return new int[]{indexForNum.get(target - nums[i]), i};
+            }
+            else {
+                indexForNum.put(nums[i], i);
+            }
+
+        }
+        return null;
+    }
+}
---
+++ Offer/src/ListNode.java
@@ -0,0 +1,8 @@
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int x){
+        this.val = x;
+    }
+}
