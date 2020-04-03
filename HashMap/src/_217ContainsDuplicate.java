f173446f0ca839c069c61a617f18114ecd83851f
Elliiee <danyiluozhang@gmail.com>
2020-04-03 15:56:26 -0500

    217

---
+++ HashMap/src/_217ContainsDuplicate.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class _217ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums){
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums){
+            set.add(num);
+        }
+        return set.size() < nums.length;
+    }
+}
