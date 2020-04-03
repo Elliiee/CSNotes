90ee3c342d4fec625ca43dea65158ac566b63c86
Elliiee <danyiluozhang@gmail.com>
2020-04-02 21:17:05 -0500

    83

---
+++ LinkedList/src/_83RemoveDuplicates.java
@@ -0,0 +1,7 @@
+public class _83RemoveDuplicates {
+    public ListNode deleteDuplicates(ListNode head){
+        if (head == null || head.next == null) return head;
+        head.next = deleteDuplicates(head.next);
+        return head.val == head.next.val ? head.next : head;
+    }
+}
