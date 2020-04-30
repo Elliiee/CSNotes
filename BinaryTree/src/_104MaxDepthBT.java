5abb38e66cc6aa9cb6c75bd905e8aaee49121acb
Elliiee <danyiluozhang@gmail.com>
2020-04-30 09:51:30 -0500

    104

---
+++ BinaryTree/src/_104MaxDepthBT.java
@@ -0,0 +1,8 @@
+public class _104MaxDepthBT {
+    public int maxDepth(TreeNode root){
+        if (root == null)
+            return 0;
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
