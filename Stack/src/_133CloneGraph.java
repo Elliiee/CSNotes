02ef496a22d0fb91fa03dc057e32c232fdee089d
Elliiee <danyiluozhang@gmail.com>
2020-05-04 15:37:31 -0500

    133

---
+++ Stack/src/_133CloneGraph.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class _133CloneGraph {
+    HashMap<Node, Node> visited = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+
+        if (visited.containsKey(node))
+            return visited.get(node);
+
+        Node clonedNode = new Node(node.val, new ArrayList<>());
+        visited.put(node, clonedNode);
+
+        for (Node n : node.neighbors){
+            clonedNode.neighbors.add(cloneGraph(n));
+        }
+
+        return clonedNode;
+    }
+}
