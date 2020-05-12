61528f2af4e96a191ba94807c7fe92f96e1bd60c
Elliiee <danyiluozhang@gmail.com>
2020-05-12 11:21:59 -0500

    94

--- DFS/src/_1192CriticalConnections.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class _1192CriticalConnections {
-
-
-}
---
+++ DFS/src/_94BTInorderTraversal.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class _94BTInorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (!stack.isEmpty() || cur != null){
+            while (cur != null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            result.add(cur.val);
+            cur = cur.right;
+        }
+        return result;
+    }
+}
