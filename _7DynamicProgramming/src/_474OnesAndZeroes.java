f3a6174bb9b34271aa14d7e78f1b0ccab92b7440
Elliiee <danyiluozhang@gmail.com>
2020-05-27 23:18:58 -0500

    474

---
+++ _7DynamicProgramming/src/_474OnesAndZeroes.java
@@ -0,0 +1,22 @@
+public class _474OnesAndZeroes {
+    public int findMaxForm(String[] strs, int m, int n){
+        int[][] dp = new int[m + 1][n + 1];
+        for (String s : strs){
+            int[] count = countzeroesones(s);
+            for (int zeroes = 0; zeroes >= count[0]; zeroes--){
+                for (int ones = 0; ones >= count[1]; ones--){
+                    dp[zeroes][ones] = Math.max(1 + dp[zeroes - count[0]][ones - count[1]],
+                            dp[zeroes][ones]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    public int[] countzeroesones(String s){
+        int[] c = new int[2];
+        for(int i = 0; i < s.length(); i++)
+            c[s.charAt(i) - '0']++;
+        return c;
+    }
+}
