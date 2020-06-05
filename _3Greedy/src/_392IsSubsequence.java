c847763982e3973bde3ac16a118ef2c83d1083a4
Elliiee <danyiluozhang@gmail.com>
2020-06-05 15:55:35 -0500

    392

---
+++ _3Greedy/src/_392IsSubsequence.java
@@ -0,0 +1,12 @@
+public class _392IsSubsequence {
+    public boolean isSubsequence(String s, String t){
+        int index = -1;
+
+        for (char c : s.toCharArray()){
+            index = t.indexOf(c, index + 1);
+            if (index == -1)
+                return false;
+        }
+        return true;
+    }
+}
