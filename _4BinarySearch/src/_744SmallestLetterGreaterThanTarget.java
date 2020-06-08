32b0584056a1a35b459c76d7f341963433f23c37
Elliiee <danyiluozhang@gmail.com>
2020-06-08 16:36:22 -0500

    744

---
+++ _4BinarySearch/src/_744SmallestLetterGreaterThanTarget.java
@@ -0,0 +1,13 @@
+public class _744SmallestLetterGreaterThanTarget {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int l = 0, h = letters.length - 1;
+        while (l <= h){
+            int m = l + (h - l) / 2;
+            if (letters[m] <= target)
+                l = m + 1;
+            else
+                h = m - 1;
+        }
+        return l < letters.length ? letters[l] : letters[0];
+    }
+}
